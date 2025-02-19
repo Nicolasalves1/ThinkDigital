0708d75efd600169c33b40e8890e16d0dca8ff47
Nícolas <nicolas.souza1alves@gmail.com>
2025-02-19 22:42:44 +0000

    commit teste

--- scripts/database.js
+++ scripts/database.js
@@ -1,17 +1,17 @@
 import mysql from 'mysql2';
-
-import dotenv from 'dotenv';
-require('dotenv').config();  // Load environment variables from.env file
+ // Load environment variables from.env file ESPERO QUE DÊ CERTO PQP KKKKKKKK. nao deu certo
 
 const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,//using enviroment variables to secure sensitive information
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    host: '127.0.0.1',
+    //using enviroment variables to secure sensitive information
+    user:'root',
+    password: 'admin',//tentei usar o dotenv mas nao deu certo, tenho que tentar dnv até conseguir usar um.env
+    database: 'user_data'
 }).promise();
 
-async function getNotes(){ // Fetch all notes from the database
+async function getNotes(){ //Fetch all notes from the database
     const [rows] = await pool.query('SELECT * FROM login_data');// Get all notes from the database
     return rows; // return the fetched notes
 }
 const row = await getNotes(); //assign the function call to an object
+console.log(row); // print the fetched notes to the console
